103dd2e8384e33f409e60aa7478f383b4064cb9b
Petr Ushakov <ko3luhbka@gmail.com>
2019-04-22 22:26:47 +0300

    Create simple web server

---
+++ main.py
@@ -0,0 +1,18 @@
+import socket
+
+HOST, PORT = '', 8888
+
+listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+listen_socket.bind((HOST, PORT))
+listen_socket.listen(1)
+print('Serving HTTP on port %s ...' % PORT)
+while True:
+    client_connection, client_address = listen_socket.accept()
+    response = client_connection.send(b'Hi I\'m web server!')
+    request = client_connection.recv(1024)
+    print(request)
+    http_response = b"HTTP/1.1 200 OK\r\n\r\nHello, World!"
+    print(http_response)
+    client_connection.sendall(http_response)
+    client_connection.close()   
